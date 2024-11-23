3f63cb8d05b86ee253336d0a93ca99ea8b40de0b
mikakwr <mikakwr1007@gmail.com>
2024-11-24 07:26:37 +0900

    kadai_15を変更

---
+++ Car _Chapter15.java
@@ -0,0 +1,36 @@
+package kadai_015;
+
+//車クラス
+public class Car_Chapter15 {
+
+	//フィールド（内部データ）
+	private int gear  = 1;  //1速から5速のギアを表す
+	private int speed = 10; //10~50のスピードが出せる
+	
+	//コンストラクタ（初期化処理）
+	public  Car_Chapter15(int gear,int speed) {
+		this.gear = gear;
+		this.speed = speed;
+	}
+	
+	//【メソッド】ギアの値により速度を変える
+	//afterGearはギアチェンジ後のギアの値
+	public void gearChange(int afterGear) {
+		System.out.println("ギア" + gear + "から" + afterGear + "に切り替えました");
+	}
+	
+	//ギアチェンジ後の速度の表示
+     public void run(){
+		speed = swith(afterGear){
+			case 1 -> 10;
+			case 2 -> 20;
+			case 3 -> 30;
+			case 4 -> 40;
+			case 5 -> 50;
+			default -> 10;
+		}
+	
+	  System.out.println("速度は時速" + speed +"kmです");
+			
+		}
+	}
