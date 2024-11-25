ab59c0eb784c01db15407b29c18b832477582770
mikakwr <mikakwr1007@gmail.com>
2024-11-26 05:12:42 +0900

    afterGearを削除し、ギアチェンジ前のギア値も入力できるver.

--- Car_Chapter15_2.java
+++ Car_Chapter15_2.java
@@ -1,32 +1,14 @@
 package kadai_015;
 
-//車クラス
-public class Car_Chapter15 {
+public class CarExec_Chapter15 {
 
-	//フィールド（内部データ）
-	private int gear  = 1;  //1速から5速のギアを表す
-	private int speed = 10; //10~50のスピードが出せる
-	private int afterGear = 1; //ギアチェンジ後の１速から５速のギアを表す
+	public static void main(String[] args) {
 	
-	//コンストラクタ（初期化処理）
-	public  Car_Chapter15(int gear,int speed,int afterGear) {
-		
+	//ギアのデータを生成
+		Car_Chapter15 car1 = new Car_Chapter15(1,10);
+	
+	//車クラスのgearChangeメソッドを実行
+		car1.run(3);
+		car1.gearChange(5);		
 	}
-	
-	//【メソッド gearChange】ギアの値により速度を変える
-	public void gearChange(int afterGear) {
-		 this.afterGear=afterGear;
-         speed = switch(afterGear){
-			case 1 -> 10;
-			case 2 -> 20;
-			case 3 -> 30;
-			case 4 -> 40;
-			case 5 -> 50;
-			default -> 10;
-		};
-		
-		System.out.println("ギア" + gear + "から" + afterGear + "に切り替えました");
-		System.out.println("速度は時速" + speed + "kmです");
-	}
-	
 }
